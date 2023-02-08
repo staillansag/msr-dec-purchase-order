9fe8c436ebbf0fd21b9eb3bd791a0537393a078d
staillansag <103417302+staillansag@users.noreply.github.com>
2023-02-08 21:12:44 +0100

    Adding assets for automated testing

--- resources/buildScripts/88.automatedTests.sh
+++ resources/buildScripts/88.automatedTests.sh
@@ -1,4 +1,12 @@
  #!/bin/bash
 
+echo "Getting Postman environment file for Production environment"
+if [ ! -f "${POSTMANENVIRONMENTPRODMSR_SECUREFILEPATH}" ]; then
+    echo "Secure file path not present: ${POSTMANENVIRONMENTPRODMSR_SECUREFILEPATH}"
+    exit 1
+fi
+
+newman run ./resources/test/DecPurchaseOrders.postman_collection.json \
+    -e ${POSTMANENVIRONMENTPRODMSR_SECUREFILEPATH} || exit 8
  
 
